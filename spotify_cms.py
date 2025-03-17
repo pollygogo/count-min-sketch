66835457a06868d3065f66f08ed841b3de17bb38
Amanda Gladys Bachtiar <127319479+pollygogo@users.noreply.github.com>
2025-03-17 18:42:57 +0800

    Update spotify_cms.py

--- spotify_cms.py
+++ spotify_cms.py
@@ -4,7 +4,8 @@ import hashlib
 class SpotifyCMS(CountMinSketch):
     def __init__(self, p, error_rate, failure_probability):
         super().__init__(p, error_rate, failure_probability)
-    
+
+    # function to encode the song uri into an integer
     def song_id(self, song_uri):
         s = song_uri.strip("spotify:track:")
         h = int(hashlib.md5(s.encode()).hexdigest(), 16) % self.p
